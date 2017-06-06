59ad5f658d5168a42f0dfa7e59dded169791b0a5
Fantasia Overdrive <1134693876@qq.com>
2017-06-06 22:42:59 +0800

    Add files via upload

---
+++ Queen.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 14 13:31:37 2017
+
+@author: Administrator
+"""
+import pygame
+import random
+import classroom
+import time
+class Queen:
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+class Chessboard(classroom.Classroom):
+    def setbegin(self):
+        self.queens=[]
+        for i in range(self.n):
+            r=random.randint(0,self.n-1)
+            q=Queen(i,r)
+            self.queens.append(q)
+            self.a[i][r]=1
+    def show(self):
+        for l in self.a:
+            print l
+    def is_fight(self,q1,q2):
+        return q1.x==q2.x or q1.y==q2.y or abs(q1.x-q2.x)==abs(q1.y-q2.y)
+    def how_fight(self,q,qq):
+        lis=0
+        for i in [s for s in self.queens if s!=qq]:
+            if self.is_fight(q,i):
+                lis+=1
+        return lis
+    def move(self,q):
+        futs=[Queen(x,y) for x in [(q.x)+1,q.x,(q.x)-1] for y in [(q.y)+1,q.y,(q.y)-1] if x<self.n if y<self.n if x>=0 if y>=0 if self.a[x][y]!=1]
+        minf=q
+        for qu in futs:
+            if self.how_fight(qu,q)<self.how_fight(minf,q):
+                minf=qu
+        if self.how_fight(q,q)==0:
+            pass
+        elif self.how_fight(q,q)>0:
+            q=minf
+            self.makea()
+    def update(self):
+        for q in self.queens:
+            self.move(q)
+    def show(self):
+        for l in self.a:
+            print l
+        print ''
+    def safe(self):
+        l=[q for q in self.queens if self.how_fight(q,q)==0]
+        return l==[]
+    def makea(self):
+        self.a=[]
+        for i in range(self.n):
+            self.a.append([])
+        for li in self.a:
+            for i in range(self.n):
+                li.append(0)
+        for q in self.queens:
+            self.a[q.x][q.y]=1
+BLUE=(0,0,255)
+BLACK=(0,0,0)
+ge=15
+n=100
+c=Chessboard(n)
+c.setbegin()
+#while not c.safe():
+#    c.update()
+#    c.show()
+def drawone(i,j,c):
+    if c.a[i][j]!=0:
+        pygame.draw.rect(dissurf,BLUE,(i*ge,j*ge,ge,ge))
+    else:
+        pygame.draw.rect(dissurf,BLACK,(i*ge,j*ge,ge,ge))
+def draw(n):
+    for i in range(n):
+        for j in range(n):
+            drawone(i,j,c)
+pygame.init()
+dissurf=pygame.display.set_mode((n*ge,n*ge))
+dissurf.fill(BLACK)
+while True:
+    for event in  pygame.event.get():
+        if event.type==pygame.QUIT:
+            pygame.quit()
+    c.update()
+    draw(n)
+#    time.sleep(0.2)
+    pygame.display.update()
+    
+            
+            
